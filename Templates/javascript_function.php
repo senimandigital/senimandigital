3cd8feaaf265f41c61801141a189a1921c4d25fb
Eboy <eboy_karya_amatir@yahoo.co.id>
2016-09-06 21:06:28 +0700

    Update and rename javascript.php to javascript_function.php

--- Templates/javascript_function.php
+++ Templates/javascript_function.php
@@ -1,5 +1,4 @@
-<script>
-<?php /* Menghitung karakter semisal pada textarea, fungsi otomatis menempatkan posisi count pada sudut kanan bawah textarea.
+<?php /* Menghitung karakter semisal pada textarea: fungsi otomatis menempatkan posisi count pada sudut kanan bawah textarea.
 <div style="position:relative;">
   <textarea id="text" onKeyUp="hitung_karakter(this)" data-counter-id="counter"></textarea>
   <div id="counter" style=" position:absolute; right:10; bottom:5">12</div>
@@ -7,5 +6,5 @@
 */ ?>
 function hitung_karakter($objek){ 
    document.getElementById($objek.getAttribute('data-counter-id')).innerHTML =  
-   document.getElementById($objek.getAttribute('id')).value.length; }
-</script>
+   document.getElementById($objek.getAttribute('id')).value.length;
+}
