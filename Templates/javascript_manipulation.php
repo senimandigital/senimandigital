56892b495df927e4c9c15cded5708edfe97cb9c6
Eboy <eboy_karya_amatir@yahoo.co.id>
2016-09-06 21:10:11 +0700

    Create javascript_manipulation.php

---
+++ Templates/javascript_manipulation.php
@@ -0,0 +1,11 @@
+<?php /* Otomatis mengambil konten meta title dan description halaman, kemudian menambahkan-nya sebagai judul formulir */ ?>
+document.querySelectorAll('main header')[0].innerHTML  = '<h1>' + document.querySelectorAll('main header')[0].getAttribute('title') + '</h1>';
+document.querySelectorAll('main header')[0].innerHTML += document.querySelectorAll('meta[name=description]')[0].getAttribute('content');
+
+
+<?php /* Otomatis menambahkan karakter bintang untuk elemen input yang terdefinisi Requred. */ ?>
+var $input = document.getElementsByTagName('input'); var $label = document.getElementsByTagName('label');
+for(i = 0 ; i < $input.length ; i++) { if ($input.item(i).hasAttribute('required') !=true) continue;
+for(l = 0 ; l < $label.length ; l++) { if ($label.item(l).getAttribute('for') != $input.item(i).getAttribute('name')) continue;
+$label.item(l).innerHTML = $label.item(l).innerHTML + '<span class="required"> *</span>';
+}}
